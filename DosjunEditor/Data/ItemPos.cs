f3bac3a0574924ae03e285e041b89d7457b70738
Paul Davies <lagdotcom@gmail.com>
2018-06-30 21:43:30 +0100

    added TileX/TileY fields to ItemPos for later

--- DosjunEditor/Data/ItemPos.cs
+++ DosjunEditor/Data/ItemPos.cs
@@ -9,6 +9,8 @@ namespace DosjunEditor
             ItemId = br.ReadUInt16();
             X = br.ReadByte();
             Y = br.ReadByte();
+            TileX = br.ReadByte();
+            TileY = br.ReadByte();
         }
 
         public void Write(BinaryWriter bw)
@@ -16,10 +18,14 @@ namespace DosjunEditor
             bw.Write(ItemId);
             bw.Write(X);
             bw.Write(Y);
+            bw.Write(TileX);
+            bw.Write(TileY);
         }
 
         public ushort ItemId { get; set; }
         public byte X { get; set; }
         public byte Y { get; set; }
+        public byte TileX { get; set; }
+        public byte TileY { get; set; }
     }
 }
