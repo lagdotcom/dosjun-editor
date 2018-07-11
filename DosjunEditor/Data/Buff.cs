92fd06c825124d04b8e3fddb531c80d75142ef4d
Paul Davies <lagdotcom@gmail.com>
2018-07-11 08:05:44 +0100

    new buff structure

--- DosjunEditor/Data/Buff.cs
+++ DosjunEditor/Data/Buff.cs
@@ -4,21 +4,27 @@ namespace DosjunEditor
 {
     public class Buff : IBinaryData
     {
-        public const int Size = 4;
+        public const int Size = 8;
 
         public void Read(BinaryReader br)
         {
             Type = (BuffType)br.ReadUInt16();
-            Argument = br.ReadInt16();
+            Duration = br.ReadInt16();
+            Argument1 = br.ReadInt16();
+            Argument2 = br.ReadInt16();
         }
 
         public void Write(BinaryWriter bw)
         {
             bw.Write((ushort)Type);
-            bw.Write(Argument);
+            bw.Write(Duration);
+            bw.Write(Argument1);
+            bw.Write(Argument2);
         }
 
         public BuffType Type { get; set; }
-        public short Argument { get; set; }
+        public short Duration { get; set; }
+        public short Argument1 { get; set; }
+        public short Argument2 { get; set; }
     }
 }
