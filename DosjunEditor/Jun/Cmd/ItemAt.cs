6473539be17a3d85ce3619fc5a4648c846ad868c
Paul Davies <lagdotcom@gmail.com>
2018-10-22 07:59:15 +0100

    added ItemAt

---
+++ DosjunEditor/Jun/Cmd/ItemAt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace DosjunEditor.Jun.Cmd
+{
+    class ItemAt : ICmd
+    {
+        private readonly Argument[] args = new Argument[]
+        {
+            new Argument("Item", ArgumentType.Item),
+            new Argument("X", ArgumentType.Number),
+            new Argument("Y", ArgumentType.Number),
+        };
+
+        public bool IsGlobal => false;
+        public bool IsScript => true;
+        public string Name => nameof(ItemAt);
+        public Argument[] Args => args;
+        public Argument Returns => Argument.Success;
+        public Op Op => Op.ItemAt;
+
+        public void Apply(Parser p, Dictionary<string, Token> a)
+        {
+            p.EmitArgument(a["Item"]);
+            p.EmitArgument(a["X"]);
+            p.EmitArgument(a["Y"]);
+            p.Emit(Op);
+        }
+    }
+}
--- DosjunEditor/Jun/Op.cs
+++ DosjunEditor/Jun/Op.cs
@@ -79,5 +79,6 @@
         Listen,
         HasItem,
         TakeItem,
+        ItemAt,
     }
 }
